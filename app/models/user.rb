357fb1209287f748d45a944eccad5dae815d52ad
Dillon Flamand <Dillon@Dillons-MacBook-Pro.local>
2016-11-02 18:31:39 -0700

    added bcrypt has_secure_password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
 	before_save {self.email = self.email.downcase}
 
 	EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-	
+
 	validates :email, :presence => true, :uniqueness => { :case_sensitive => false }, :length => {maximum: 60}, :format => {with: EMAIL_REGEX}
 	validates :password, :presence => true
+
+	has_secure_password
 end
