bfe7e9ad29089f0558b6ff64bb73f11bb39719b4
Dillon Flamand <Dillon@Dillons-MacBook-Pro.local>
2016-11-02 20:12:50 -0700

    user new action complete, create action started

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   end
+
+  def create
+  	@user = User.new(get_user_params)
+
+  	if @user.save
+  		# do stuff
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+
+	  def get_user_params
+	  	params.require(:user).permit(:email, :firstName, :lastName, :password, :password_confirmation)
+	  end
 end
