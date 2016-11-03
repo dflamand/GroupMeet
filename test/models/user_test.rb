330dbe257cc6064c87a8e48e62d5c5eff2b80e24
Dillon Flamand <Dillon@Dillons-MacBook-Pro.local>
2016-11-02 17:33:18 -0700

    test for email/password presence and email uniqueness

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,33 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "email present" do
+  	@user = User.new
+  	@user.email = "     "
+  	@user.password = "password"
+
+  	assert_not @user.valid?
+  end
+
+  test "password present" do
+  	@user = User.new
+  	@user.email = "email@test.ca"
+  	@user.password = "   "
+
+  	assert_not @user.valid?	
+  end
+
+  test "duplicate email" do
+  	@first = User.new
+  	@first.email = "email@test.com"
+  	@first.password = "password"
+  	@first.save
+
+  	@second = User.new
+  	@second.email = "email@test.com"
+  	@first.password = "password"
+
+  	assert_not @second.valid?
+  end
 end
