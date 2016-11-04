d7458aaebe02658d0d62ddb4a1cc48e5e54b147d
Dillon Flamand <Dillon@Dillons-MacBook-Pro.local>
2016-11-03 22:12:20 -0700

    changed routes for login/signup/logout to have a prettier url

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
-  root 'users#new'
+  get '/signup', to: 'users#new'
+
+
+  root 'sessions#new'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
