bd69c50509cb6e95f712593d698ca328b1eeccd7
Dillon Flamand <Dillon@Dillons-MacBook-Pro.local>
2016-11-03 22:47:47 -0700

    get current logged in user, and check if a user is logged in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,4 +3,16 @@ module SessionsHelper
 	def login(user)
 		session[:userID] = user.id
 	end
+
+	def currentUser
+		if @currentUser.nil?
+			@currentUser = User.find_by(id: session[:userID])
+		else
+			@currentUser
+		end
+	end
+
+	def loggedIn?
+		!currentUser.nil?
+	end
 end
