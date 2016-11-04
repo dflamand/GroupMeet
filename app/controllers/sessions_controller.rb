91a34904cf448384250bb7885725381a324599e0
Dillon Flamand <Dillon@Dillons-MacBook-Pro.local>
2016-11-03 22:37:50 -0700

    created login form and authenticated login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	user = User.find_by(email: params[:user][:email])
+  	if user && user.authenticate(params[:user][:password])
+  		redirect_to '/map.html'
+  	else
+  		render 'new'
+  	end
+  end
+
+  
 end
