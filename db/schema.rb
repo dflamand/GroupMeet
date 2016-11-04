8cef750af67be9bd268fd8cebeac5f2a24b28232
Bjorn <jkerssen@sfu.ca>
2016-11-03 19:18:22 -0700

    added index in user for groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161104021445) do
+ActiveRecord::Schema.define(version: 20161104021736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20161104021445) do
     t.datetime "updated_at",      null: false
     t.integer  "group_id"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["group_id"], name: "index_users_on_group_id", using: :btree
   end
 
   add_foreign_key "locations", "groups"
