769fc2e09fb0b436f2c36c7dadde31be6000bb2a
Dillon Flamand <Dillon@Dillons-MacBook-Pro.local>
2016-11-03 22:39:19 -0700

    save a cookie after successful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,12 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(email: params[:user][:email])
   	if user && user.authenticate(params[:user][:password])
+  		login user
   		redirect_to '/map.html'
   	else
   		render 'new'
   	end
   end
 
-  
+
 end
