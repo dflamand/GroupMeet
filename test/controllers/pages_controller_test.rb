412f4d6cfd74c3d609d84bfdc39ddd2976ef5b6c
dflamand <dflamand@users.noreply.github.com>
2016-11-06 17:46:27 -0800

    some controller tests success

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "get root" do 
+		get root_url
+		assert_response :success
+	end
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,5 +2,14 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   
+	test "login" do
+		get login_url
+		assert_response :success
+	end
+
+	test "logout" do
+		get logout_url
+		assert_redirected_to root_url
+	end
 
 end
