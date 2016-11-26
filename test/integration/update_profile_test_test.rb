49c011621a3c7ee08e14ca3a47554eedde6f29bd
Dillon Flamand <dflamand@users.noreply.github.com>
2016-11-26 14:25:53 -0800

    integration test for update user profile

---
+++ test/integration/update_profile_test_test.rb
@@ -0,0 +1,45 @@
+require 'test_helper'
+
+class UpdateProfileTestTest < ActionDispatch::IntegrationTest
+  
+  test "change name/email" do
+
+  	@user = User.create(:firstName => "first", :lastName => "last", :email => "test@user.com", :password => "password", :password_confirmation => "password")
+
+	get root_url
+	assert_response :success
+
+	post login_url, params:{ user: {email: "test@user.com", password: "password"}}
+
+	assert_response :redirect
+	assert_redirected_to root_url
+	follow_redirect!
+
+	assert_select 'a', 'Log Out'
+
+	assert_select 'li', 'Update Profile'
+
+	patch user_url(@user), params:{ user: {firstName: "Fchanged", lastName: "Lchanged", email: "changed@email.com"}}
+
+	assert_response :redirect
+	assert_redirected_to root_url
+	follow_redirect!
+
+	assert_select 'li[class=list-group-item]', 'Name: FchangedLchanged'
+
+	delete logout_url
+
+	assert_response :redirect
+	assert_redirected_to root_url
+	follow_redirect!
+
+	post login_url, params:{ user: {email: "changed@email.com", password: "password"}}
+
+	assert_response :redirect
+	assert_redirected_to root_url
+	follow_redirect!
+
+	assert_select 'li[class=list-group-item]', 'Email: changed@email.com'
+
+  end
+end
