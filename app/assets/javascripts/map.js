9de069edda66c75a5e7a32a5d7e1bf20893d94f8
Daniel Siemens <danielpsiemens@gmail.com>
2016-11-26 19:12:27 -0800

    Fixed transport html

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -108,9 +108,9 @@ function addAddressHTML() {
   addrCount++;
   var addrStr = 'addr' + addrCount;
 
-  var newHTML = '<div class="address"> <div class="address-header">' + 'Address ' + addrCount +'</div> <div class="transport-options"> <a class="carMode"><i class="fa fa-car"></i></a><a class="transitMode"><i class="fa fa-subway"></i></a> <a class="walkMode"><i class="fa fa-male"></i></a><a class="bicycleMode"><i class="fa fa-bicycle"></i></a></div>' +
+  var newHTML = '<div class="address"> <div class="address-header">' + 'Address ' + addrCount +'</div> <div class="transport-options"> <a class="carMode"><i class="fa fa-car"></i></a> <a class="transitMode"><i class="fa fa-subway"></i></a> <a class="walkMode"><i class="fa fa-male"></i></a> <a class="bicycleMode"><i class="fa fa-bicycle"></i></a></div>' +
   '<div class="input-group"><span class="input-group-addon"><input type="checkbox" name="' + addrStr + '"checked></span><input id="' + addrStr + '" type="text" class="form-control addrInput" name="' + addrStr + '" placeholder="Address ' + addrCount + '"></div>'
-  + '<div class="row tripInfo"><div class="tripDuration col-md-6"><span class="glyphicon glyphicon-hourglass" aria-hidden="true"></span> 10 Minutes </div> <div class="tripDistance col-md-6"> <span class="glyphicon glyphicon-flag" aria-hidden="true"></span> 10 KM </div> </div> </div><hr>';
+  + '<div class="row tripInfo"><div class="tripDuration col-md-6"><span class="glyphicon glyphicon-hourglass" aria-hidden="true"></span><span id="timeText"> 10 Minutes </span> </div> <div class="tripDistance col-md-6"> <span class="glyphicon glyphicon-flag" aria-hidden="true"></span><span id="distanceText"> 10 KM </span></div> </div> </div><hr>';
 
   $( "#addressList" ).append(newHTML);
   console.log(addrStr);
