4a1a923d74e94912293239b2739fb0bebc6b9748
Dillon Flamand <dflamand@users.noreply.github.com>
2016-12-04 20:22:39 -0800

    created invite unit tests

--- test/models/invite_test.rb
+++ test/models/invite_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class InviteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "has user_id" do
+  	@I = Invite.new
+  	@I.group_id = 12
+  	@I.gname = "name"
+
+  	assert_not @I.valid?
+
+  end
+
+  test "has group_id" do
+  	@I = Invite.new
+  	@I.user_id = 12
+  	@I.gname = "name"
+
+  	assert_not @I.valid?
+  end
+
+  test "has gname" do
+  	@I = Invite.new
+  	@I.group_id = 12
+  	@I.user_id = 12
+
+  	assert_not @I.valid?
+
+  end
 end
